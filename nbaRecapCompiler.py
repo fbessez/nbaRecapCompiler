6aa1b6cbc90d1f3b2a0d9bc44bc2424daa234a16
Fabien Bessez <fabien.bessez@gmail.com>
2017-11-07 13:33:40 -0500

    adding ydl_opts and attempted speed-up

--- nbaRecapCompiler.py
+++ nbaRecapCompiler.py
@@ -2,47 +2,67 @@ from __future__ import unicode_literals
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from datetime import date, timedelta
-# from time import sleep
 from subprocess import call
 import sys
+# import os
 import youtube_dl
 
+
 def soupify(url):
-	driver = webdriver.PhantomJS()
-	driver.get(url)
+    driver = webdriver.PhantomJS()
+    driver.get(url)
 
-	data = driver.page_source
-	driver.close()
+    data = driver.page_source
+    driver.close()
 
-	return BeautifulSoup(data,'lxml')
+    return BeautifulSoup(data,'lxml')
 
-def find_days_games(soup, days_ago=1):
-	game_sections = soupify(url).findAll('section', {'class': 'tile'})
-	correct_urls = []
-	base_url = 'http://www.nba.com'
-	yesterdays_date = (date.today() - timedelta(days_ago)).strftime('%Y/%m/%d')
-	for game in game_sections:
-		if yesterdays_date in game['data-video-id']:
-			correct_urls.append(base_url + game['data-video-id'])
-	return correct_urls
+def find_days_games(soup, date):
+    game_sections = soupify(url).findAll('section', {'class': 'tile'})
+    correct_urls = []
+    base_url = 'http://www.nba.com'
+    for game in game_sections:
+        if date in game['data-video-id']:
+            correct_urls.append(base_url + game['data-video-id'])
+    return correct_urls
 
+
+def my_hook(d):
+    if d['status'] == 'finished':
+        print('Finished downloading %s' % d['filename'])
+        print("Filename: %s" % d['filename'])
 
 def download_games(game_urls):
-	ydl_opts = {}
-	youtube_dl.YoutubeDL(ydl_opts).download(game_urls)
+    ydl_opts = {
+    "forcetitle": True,
+    "quiet": True,
+    "outtmpl": "%(title)s.%(ext)s",
+    "progress_hooks": [my_hook],
+    "nooverwrites": True,
+    "no_warnings": True
+    }
+    meta = youtube_dl.YoutubeDL(ydl_opts).extract_info(game_urls[0], download=False)
+    print 'upload date : %s' %(meta['upload_date'])
+    print 'id          : %s' %(meta['id'])
+    print 'format      : %s' %(meta['format'])
+    print 'duration    : %s' %(meta['duration'])
+    print 'title       : %s' %(meta['title'])
+    print 'description : %s' %(meta['description'])
+    youtube_dl.YoutubeDL(ydl_opts).download(game_urls)
+
 
 
 if __name__ == '__main__':
-	try:
-		days_ago = int(sys.argv[1])
-	except:
-		days_ago = 1
-	url = 'http://www.nba.com/video/gamerecaps#/'
-	soup = soupify(url)
-	game_urls = find_days_games(soup, days_ago)
-	download_games(game_urls)
-	call('open *.mp4', shell=True)
-	# call('rm *.mp4', shell=True)
+    try:
+        days_ago = int(sys.argv[1])
+    except:
+        days_ago = 1
+    date = (date.today() - timedelta(days_ago)).strftime('%Y/%m/%d')
+    url = 'http://www.nba.com/video/gamerecaps#/'
+    soup = soupify(url)
+    game_urls = find_days_games(soup, date)
+    download_games(game_urls)
+    call('open *.mp4', shell=True)
 
 
 
